2e9bb55453402fee740483f59bbabd78986217d5
Drewpiter39 <drew.turgeon@maine.edu>
2025-11-22 19:17:47 -0500

    Printing unicode
    I just don't want to lose all this when my computer dies

--- 226HashTree.py
+++ 226HashTree.py
@@ -1,19 +1,30 @@
 class DataItem:
     def __init__(self, line):
-        # self.movieName
-        #self.genre
-        # self.releaseDate
-        # self.director
-        # self.revenue
-        # self.rating
-        # self.minDuration
-        # self.productionCompany
-        # self.quote
-        pass 
+        # Attrubutes of each movie/line
+        self.movieName = line[0] # Name
+        self.genre = line[1] # Genre
+        self.releaseDate = line[2] # Date
+        self.director = line[3] # Director
+        self.revenue = line[4] # Money
+        self.rating = line[5] # Score
+        self.minDuration = line[6] # Length
+        self.productionCompany = line[7] # Company
+        self.quote = line[8] # Quote
 
+# Turns the the string into its unicode variant
 def hashFunction(stringData):
     # Do things to stringData, turn into an int
-    pass
+    # The thing that stores the turn
+    turn = 0
+    # For each character in the string it will cycle
+    for char in stringData:
+        # Adds the unicode to turn
+        turn = (turn + ord(char))
+    # Returns the unicode variant
+    return turn
+
+
+
 
 import csv
 size = 10000
@@ -26,9 +37,11 @@ counter = 0
 with open(file, 'r', newline = '', encoding = "utf8") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        newItem = DataItem(row)
+        # newItem = DataItem(row)
 
-        print(row)
+        # print(row[0])
+        print(hashFunction(row[0]))
+
         # Create a DataItem from row
         # feed the appropriate field into the hash function
         # to get a key
@@ -37,3 +50,11 @@ with open(file, 'r', newline = '', encoding = "utf8") as csvfile:
         # handle any collisions
         counter += 1
 print(counter)
+
+# import random
+
+# def createList(n): # creates a random list of size n
+#     x = []
+#     for i in range(n):
+#         x.append(random.randint(1, 10000))
+#     return x
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+Name: Drew N. Turgeon
+Due Date: 11/25/2025
+Class: COS 226
+Teacher: Professor Schotter
